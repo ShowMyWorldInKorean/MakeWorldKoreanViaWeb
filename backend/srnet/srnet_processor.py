cde6598b0959c82c4b6306566123e872cfd43654
10wook <younguk137@naver.com>
2025-08-10 21:47:54 +0900

    hyw/env_fix_font

--- backend/srnet/srnet_processor.py
+++ backend/srnet/srnet_processor.py
@@ -82,27 +82,28 @@ def generate_i_t():
     image_size = '256x128'
 
     gray_bg_path = f'{SAVE_PATH}/i_t_utils/gray_bg_256x128.png'
-    font_name = 'Noto Sans'
+    # Noto Sans CJK KR (한글 지원)
+    font_name = 'Noto Sans CJK KR'
 
     for crop_name, label in tqdm(labels.items()):
-        
         label = label['trans_txt']
-        
+
         save_file_path = os.path.join(save_path, f'{crop_name}.png')
         temp_save_file_path = os.path.join(temp_save_path, f'{crop_name}.png')
-        
-        input_text_command = f'convert -alpha set -background "rgb(121,127,141)" pango:\'  \
-                            <span font_stretch="semicondensed" foreground="#000000" font=" {font_name} 30 ">{label.lower()}</span> \
-                            \' \png:-|convert -  \\( +clone \\) +swap  -background "rgb(121,127,141)" -layers merge  +repage png:-|\
-                            convert -   -trim +repage -resize {text_image_size} {temp_save_file_path}'
+
+        # pango에서 한글 폰트 강제 지정
+        input_text_command = f'convert -alpha set -background "rgb(121,127,141)" pango:\'\
+<span font_stretch="semicondensed" foreground="#000000" font="{font_name} 30">{label}</span>\
+\' png:-|convert -  \\( +clone \\) +swap -background "rgb(121,127,141)" -layers merge +repage png:-|\
+convert - -trim +repage -resize {text_image_size} {temp_save_file_path}'
 
         os.system(input_text_command.encode('utf-8'))
-                            
-        finalInputTextCommand = 'composite -gravity Center ' + f'{temp_save_file_path}' + ' ' + gray_bg_path
-        finalInputTextCommand += ' png:-|'
-        finalInputTextCommand += 'convert -  ' + save_file_path
+
+        finalInputTextCommand = f'composite -gravity Center {temp_save_file_path} {gray_bg_path} png:-|convert - {save_file_path}'
         os.system(finalInputTextCommand.encode('utf-8'))
+
     os.system(f'rm -rf {temp_save_path}')
+
 
 
 def make_masks():
